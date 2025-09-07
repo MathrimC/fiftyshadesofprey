c28992f2a1dabe5dc444622346a120eadd77f060
mathrim <mathrimcpoker@gmail.com>
2025-09-07 17:24:13 +0200

    naming new dinosaurs

--- scenes/incubator/incubator.gd
+++ scenes/incubator/incubator.gd
@@ -1,19 +1,22 @@
 class_name Incubator
 extends Control
 
-@export var container: GridContainer
+@export var container: Container
+@export var empty_label: Label
 
 func _ready() -> void:
 	for dinosaur in game_manager.game_data.eggs:
 		var incubatoregg: Incubatoregg = load(Resources.scenes[Resources.Scene.INCUBATOREGG]).instantiate()
 		incubatoregg.dinosaur = dinosaur
 		container.add_child(incubatoregg)
+	empty_label.visible = game_manager.game_data.eggs.is_empty()
 	game_manager.egg_created.connect(on_egg_created)
 
 func _dinosaur_button_pressed() -> void:
 	game_manager.switch_scene(Resources.Scene.DINOS)
 
 func on_egg_created(dinosaur: DinosaurInstance) -> void:
+	empty_label.hide()
 	var incubatoregg: Incubatoregg = load(Resources.scenes[Resources.Scene.INCUBATOREGG]).instantiate()
 	incubatoregg.dinosaur = dinosaur
 	container.add_child(incubatoregg)
--- scenes/incubator/place_egg.gd
+++ scenes/incubator/place_egg.gd
@@ -2,12 +2,19 @@ class_name PlaceEgg
 extends Node
 
 @export var map: Map
+@export var dinosaur_naming: Container
 
 var dinosaur: DinosaurInstance
 
 func _ready() -> void:
+	dinosaur_naming.hide()
 	map.lot_selected.connect(on_lot_selected)
 
 func on_lot_selected(lot_number: int) -> void:
 	if game_manager.place_dinosaur(dinosaur, lot_number):
-		game_manager.switch_scene(Resources.Scene.INCUBATOR)
+		dinosaur_naming.show()
+
+func on_name_submitted(dino_name: String) -> void:
+	dinosaur.name = dino_name
+	game_manager.switch_scene(Resources.Scene.DINOPARK)
+	
